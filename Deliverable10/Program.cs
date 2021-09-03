2106486d80090057d587b820f7e67e8c301d59de
gabi <gabriella.marias@gmail.com>
2021-09-03 12:51:23 -0400

    Second commit for deliverable ten, updated and completed code for exercise 49

--- Deliverable10/Program.cs
+++ Deliverable10/Program.cs
@@ -11,10 +11,17 @@ namespace Deliverable10
         {
             public int size { get; set; }
             public Square(int size)
-            {
+            {  
                 this.size = size;
             }
-            
+
+        }
+
+        public static int squareCount(List<Square> countSquare)
+        {
+            var count = countSquare.Count();
+
+            return count;
         }
 
         public static int Largest(List <Square> largestSquare)
@@ -31,41 +38,70 @@ namespace Deliverable10
         }
         public static double averageArea(List<Square> averageAreaSquare)
         {
-          
-       
+            List<int> intSquare = new List<int>();
 
-        public static int averagePerimeter(List<Square> averagePerimeterSquare)
+            foreach (Square s in averageAreaSquare)
+            {
+                int i = s.size * s.size;
+
+                intSquare.Add(i);
+            }
+
+            return intSquare.Average();
+        }
+
+
+        public static double averagePerimeter(List<Square> averagePerimeterSquare)
         {
-            
+            List<int> intSquare = new List<int>();
+
+            foreach (Square s in averagePerimeterSquare)
+            {
+                int i = s.size * 4;
+
+                intSquare.Add(i);
+            }
+
+            return intSquare.Average();
         }
 
         public static void Main(string[] args)
         {
             List <Square> squares = new List<Square>();
             int userNumberInput = 0;
+            string userInputContinue = "";
             bool userContinue = true;
+            
 
             while (userContinue == true)
             {
                 Console.Write("Enter a side length (q to quit): ");
 
-                if (int.TryParse(Console.ReadLine(), out userNumberInput))
+                var userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out userNumberInput))
                 {
                     Square s = new Square(userNumberInput);
                     squares.Add(s);
-
-                    foreach (Square c in squares)
-                    {
-                        Console.WriteLine(c.size);
-                    }
                 }
 
-                else  
+                else if (userInput == "q")
                 {
-                    
-                    Console.WriteLine(Largest(squares));
-                    Console.WriteLine(Smallest(squares));
-                    Console.WriteLine(averageArea(squares));
+                    Console.WriteLine($"You created {squareCount(squares)} squares.");
+                    Console.WriteLine($"Largest: {Largest(squares)}");
+                    Console.WriteLine($"Smallest: {Smallest(squares)}");
+                    Console.WriteLine($"Average Area: {averageArea(squares)}");
+                    Console.WriteLine($"Average Perimeter: {averagePerimeter(squares)}");
+                    Console.Write("Do you wish to continue (y/n)? ");
+
+                    userInputContinue = Console.ReadLine();
+                    if (userInputContinue == "n")
+                    {
+                        userContinue = false;
+                        Console.WriteLine("Goodbye!");
+                    }
+
+                    squares.Clear();
 
                 }
 
